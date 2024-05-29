1a0faf54f1b740efb5eead3387b83d94c38357d9
SaintDrew <drew_el12@yahoo.com>
2024-05-29 04:36:19 -0400

    Adding renders and routes

--- app.js
+++ app.js
@@ -1,34 +1,49 @@
-const express = require('express')
-const app = express()
-const http = require('http')
-const port = 3000
+const express = require('express');
+const app = express();
+// const http = require('http')
+// const port = 3000
 ///////////////////////////////////////////////////////////
-app.set("views", "views");
+// app.set("views", "views");
 ///////////////////////////////////////////////////////////
 app.set("view engine", "ejs");
 ///////////////////////////////////////////////////////////
-app.use(express.json());
-/**
+// app.use(express.json());
 
-Middleware*/
-const logReq = (request, response, next) => {
-    console.log(`${request.method} was made to ${request.url} from the hostname: ${request.hostname}!!!`
-    );
-    console.log("Request received!!!");
-    next();
-};
-app.use(logReq);
-app.use(express.urlencoded({ extended: true }));
-//Parsing Midware
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json({ extended: true }));
+// Listening for requests 
+app.listen(3000)
 
-// Error-handling middleware.
-app.use((error, request, response, next) => {
-    response.status(error.status || 500);
-    response.json({ errMessage: error.message });
+app.get('/', (req, res) => {
+    res.render('index');
 });
 
-app.listen(port, () => {
-    console.log(`This server is listening on port: ${port}`);
+app.get('/about', (req, res)=> {
+    res.render('about');
 });
+
+
+
+
+// /**
+
+// Middleware*/
+// const logReq = (request, response, next) => {
+//     console.log(`${request.method} was made to ${request.url} from the hostname: ${request.hostname}!!!`
+//     );
+//     console.log("Request received!!!");
+//     next();
+// };
+// app.use(logReq);
+// app.use(express.urlencoded({ extended: true }));
+// //Parsing Midware
+// app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(bodyParser.json({ extended: true }));
+
+// // Error-handling middleware.
+// app.use((error, request, response, next) => {
+//     response.status(error.status || 500);
+//     response.json({ errMessage: error.message });
+// });
+
+// app.listen(port, () => {
+//     console.log(`This server is listening on port: ${port}`);
+// });
