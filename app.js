bc9b42881e00b64b4fb90fa252f4e3df8a48d720
SaintDrew <drew_el12@yahoo.com>
2024-05-30 05:18:08 -0400

    Middleware

--- app.js
+++ app.js
@@ -12,6 +12,13 @@ app.set("view engine", "ejs");
 // Listening for requests 
 app.listen(3000)
 
+app.use((req, res) => {
+    console.log('new request made:');
+    console.log('host: ', req.hostname);
+    console.log('path: ', req.path);
+    console.log('method: ', req.method);
+});
+
 app.get('/', (req, res)=> {
     const blogs = [
         {title: 'Yoshi finds eggs', snippet: 'lorem ipsum dolor sit amet consectetur'},
@@ -21,16 +28,16 @@ app.get('/', (req, res)=> {
     res.render('index', { title: 'Home', blogs });
 });
 
-app.get('/about', (req, res)=> {
+app.get('/about', (req, res) => {
     res.render('about', { title: 'About' });
 });
 
-app.get('/blogs/create', (req, res)=> {
+app.get('/blogs/create', (req, res) => {
     res.render('create', { title: 'Create New Blog' });
 });
 
 // 404 page
-app.use((req, res)=> {
+app.use((req, res) => {
     res.status(404).render('404', { title: '404' });
 });
 
